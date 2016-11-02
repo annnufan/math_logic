e3588c5d0593e09d75c74ec29e5221a60e447ddd
annnufan <annnufan@gmail.com>
2016-11-02 22:12:32 +0300

    getline

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+ifstream fin("in.in");
 ofstream fout("out.out");
 
 string to_string(string s) {
@@ -20,10 +21,8 @@ string to_string(string s) {
 
 string to_string_int(int x) {
 	string s = "";
-	
 	while (x > 0) {
 		char c = (x % 10) + '0';
-		//cout << endl << x % 10 << ' ' <<c << endl; 
 		s = c + s;
 		x /= 10;	
 	}
@@ -31,8 +30,6 @@ string to_string_int(int x) {
 		s = "0";
 	return s;
 }
-
-
 
 struct expression {
 	expression(string s) {
@@ -53,52 +50,57 @@ struct expression {
 			return;	
 		}
 		
-		i = 0;
+		i = s.length() - 1;
 		balance = 0;
-		while (i < s.length() && (s[i] != '|' || balance != 0)) {
+		while (i >= 0 && (s[i] != '|' || balance != 0)) {
 			if (s[i] == '(')
-				balance++;
-			if (s[i] == ')')
 				balance--;
-			i++;
+			if (s[i] == ')')
+				balance++;
+			i--;
 		}
-		if (i != s.length()) {
+		if (i != -1) {
 			type = 1;
 			first = new expression(s.substr(0, i));
 			second = new expression(s.substr(i + 1));
 			return;					
 		}
 		
-		i = 0;
+		i = s.length() - 1;
 		balance = 0;
-		while (i < s.length() && (s[i] != '&' || balance != 0)) {
+		while (i >= 0 && (s[i] != '&' || balance != 0)) {
 			if (s[i] == '(')
-				balance++;
-			if (s[i] == ')')
 				balance--;
-			i++;
+			if (s[i] == ')')
+				balance++;
+			i--;
 		}
-		if (i != s.length()) {
+		
+		if (i != -1) {
 			type = 2;
 			first = new expression(s.substr(0, i));
 			second = new expression(s.substr(i + 1));
 			return;					
 		}
+		
 		if (s[0] == '!') {
 			type = 3;
 			first = new expression(s.substr(1));
 			second = nullptr;
 			return;	
 		}
+		
 		if(s[0] == '(') {
 			*this = expression(s.substr(1, s.length() - 1));
 			return;		
 		}
+		
 		type = 4;
 		val = s;
 		first = nullptr;
 		second = nullptr;			
 	}
+	
 	friend bool operator==(expression& a, expression& b);
 
 
@@ -113,18 +115,14 @@ bool operator==(expression& a, expression& b) {
 	return to_string(a.expr) == to_string(b.expr); 		
 }
 
-
-
 void axiom_parse(string s){
 	int i = 0;
-	//fout << s;
 	while (i < s.length() && (s[i] != '|' || s[i + 1] != '-')) {
 		string add_s = "";
 		while (i < s.length() && s[i] != ',' && (s[i] != '|' || s[i + 1] != '-')) {
 			add_s += s[i++];	
 		}
 		axioms.push_back(expression(add_s));
-		//fout << add_s << ' ' << i << ' ' << s[i] << endl;
 		if (s[i] == ',') {
 			i++;	
 		}
@@ -152,11 +150,9 @@ bool equal_tree(expression a, expression b, map<string, string> m) {
 
 string annotation(string s) {
 	expression x(s);
-	//fout << x.expr << endl;
 	evidence.push_back(x);
-		
+
 	for (int i = 0; i < (int)axioms.size(); i++) {
-		//fout << axioms[i - 1].expr << ' ' << x.expr << endl;
 		if (axioms[i] == x) {
 			string a = to_string_int(i + 1);
 			return "Предп. " + a;
@@ -191,7 +187,7 @@ string annotation(string s) {
 
 int main() {
 	string head, s;
-	cin >> head;
+	getline(fin, head);
 	expression a(head);
 	axiom_parse(head);
 	string arr[10] = {"a->b->a", "(a->b)->(a->b->c)->(a->c)", "a->b->a&b", "a&b->a", "a&b->b", "a->a|b", "b->a|b", "(a->c)->(b->c)->(a|b->c)", "(a->b)->(a->!b)->!a", "!!a->a"}; 
@@ -200,11 +196,9 @@ int main() {
 		scheme_axiom.push_back(expression(arr[i]));
 	}
 	
-		
-
 	int i = 1;
 	fout << to_string(head) << endl;
-	while (cin >> s) {
+	while (getline(fin, s)) {
 		fout << "(" << i++ << ") " << to_string(s) << " (" << annotation(s) << ")" << endl;	
 	}                                                     
 	return 0;	
